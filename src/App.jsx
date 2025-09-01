fcc57e8c5aa859952363ede9dff5eeb3e00b5ad5
Ting-Hao, Liu <s0129qkakadam0108@gmail.com>
2025-09-01 16:43:53 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+
+import "./App.css";
+import Search from "./components/Search";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+
+  return (
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper">
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>
+            Find <span className="text-gradient">Movies</span> You'll Enjoy
+            Without the Hassle
+          </h1>
+        </header>
+
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>
+    </main>
+  );
+};
+
+export default App;
