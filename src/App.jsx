8d6cc557358479a7c3bd2bbc1ce305a9d9827d3b
Ting-Hao, Liu <s0129qkakadam0108@gmail.com>
2025-09-13 00:33:25 +0800

    Modified the style of movie detail page

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,14 @@ import HomePage from './pages/HomePage';
 import MovieDetailPage from './pages/MovieDetailPage';
 
 const App = () => {
-   return (
-      <Router>
-         <Routes>
-            <Route path="/" element={<HomePage />} />
-            <Route path="/movie/:id" element={<MovieDetailPage />} />
-         </Routes>
-      </Router>
-   );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/movie/:id" element={<MovieDetailPage />} />
+      </Routes>
+    </Router>
+  );
 };
 
 export default App;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react';
 import { useDebounce } from 'react-use';
+import { Link } from 'react-router-dom';
 
 import Search from '../components/Search';
 import Spinner from '../components/Spinner';
@@ -9,160 +10,163 @@ import { updateSearchCount, getTrendingMovies } from '../appwrite';
 const API_URL = 'https://api.themoviedb.org/3';
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
 const API_OPTIONS = {
-   method: 'GET',
-   headers: {
-      accept: 'application/json',
-      Authorization: `Bearer ${API_KEY}`,
-   },
+  method: 'GET',
+  headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`,
+  },
 };
 
 const HomePage = () => {
-   const [searchTerm, setSearchTerm] = useState('');
-   const [errorMessage, setErrorMessage] = useState('');
-   const [movieList, setMovieList] = useState([]);
-   const [trendingMovies, setTrendingMovies] = useState([]);
-   const [isLoading, setIsLoading] = useState(false);
-   const [debounceSearchTerm, setDebounceSearchTerm] = useState('');
+  const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [movieList, setMovieList] = useState([]);
+  const [trendingMovies, setTrendingMovies] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [debounceSearchTerm, setDebounceSearchTerm] = useState('');
 
-   // Debounce the search term to prevent making too many API request
-   // by waiting for the user to stop typing for 500ms
-   useDebounce(
-      () => {
-         setDebounceSearchTerm(searchTerm);
-      },
-      500,
-      [searchTerm]
-   );
+  // Debounce the search term to prevent making too many API request
+  // by waiting for the user to stop typing for 500ms
+  useDebounce(
+    () => {
+      setDebounceSearchTerm(searchTerm);
+    },
+    500,
+    [searchTerm]
+  );
 
-   const fetchMovies = async (query = '') => {
-      setIsLoading(true);
-      setErrorMessage('');
+  const fetchMovies = async (query = '') => {
+    setIsLoading(true);
+    setErrorMessage('');
 
-      try {
-         const endpoint = query
-            ? `${API_URL}/search/movie?query=${encodeURIComponent(query)}`
-            : `${API_URL}/discover/movie?sort_by=popularity.desc`;
+    try {
+      const endpoint = query
+        ? `${API_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_URL}/discover/movie?sort_by=popularity.desc`;
 
-         const response = await fetch(endpoint, API_OPTIONS);
-         if (!response.ok) {
-            throw new Error('Network response was not ok');
-         }
-
-         const data = await response.json();
-         console.log(data);
-
-         if (!data.results || data.results.length === 0) {
-            setErrorMessage('No movies found');
-            setMovieList([]);
-            return;
-         }
-
-         setMovieList(data.results);
-
-         if (query && data.results.length > 0) {
-            await updateSearchCount(query, data.results[0]);
-         }
-      } catch (error) {
-         console.error('Error fetching movies:', error);
-         setErrorMessage('Failed to fetch movies, Please try again later.');
-      } finally {
-         setIsLoading(false);
+      const response = await fetch(endpoint, API_OPTIONS);
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
       }
-   };
 
-   const loadTrendingMovies = async () => {
-      try {
-         const movies = await getTrendingMovies();
+      const data = await response.json();
+      console.log(data);
 
-         setTrendingMovies(movies);
-      } catch (error) {
-         console.error('Error fetching trending movies:', error);
+      if (!data.results || data.results.length === 0) {
+        setErrorMessage('No movies found');
+        setMovieList([]);
+        return;
       }
-   };
 
-   useEffect(() => {
-      fetchMovies(debounceSearchTerm);
-   }, [debounceSearchTerm]);
+      setMovieList(data.results);
 
-   useEffect(() => {
-      loadTrendingMovies();
-   }, []);
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0]);
+      }
+    } catch (error) {
+      console.error('Error fetching movies:', error);
+      setErrorMessage('Failed to fetch movies, Please try again later.');
+    } finally {
+      setIsLoading(false);
+    }
+  };
 
-   return (
-      <>
-         <main>
-            <div className="pattern" />
+  const loadTrendingMovies = async () => {
+    try {
+      const movies = await getTrendingMovies();
 
-            <div className="wrapper">
-               <header>
-                  <img src="./hero.png" alt="Hero Banner" />
-                  <h1>
-                     Find <span className="text-gradient">Movies</span> You'll
-                     Enjoy Without the Hassle
-                  </h1>
+      setTrendingMovies(movies);
+    } catch (error) {
+      console.error('Error fetching trending movies:', error);
+    }
+  };
 
-                  <Search
-                     searchTerm={searchTerm}
-                     setSearchTerm={setSearchTerm}
-                  />
-               </header>
+  useEffect(() => {
+    fetchMovies(debounceSearchTerm);
+  }, [debounceSearchTerm]);
 
-               {trendingMovies.length > 0 && (
-                  <section className="trending">
-                     <h2>Trending Movies</h2>
+  useEffect(() => {
+    loadTrendingMovies();
+  }, []);
 
-                     <ul>
-                        {trendingMovies.map((movie, index) => (
-                           <li key={movie.$id}>
-                              <p>{index + 1}</p>
-                              <img src={movie.poster_url} alt={movie.title} />
-                           </li>
-                        ))}
-                     </ul>
-                  </section>
-               )}
+  return (
+    <>
+      <main>
+        <div className="pattern" />
 
-               <section className="all-movies">
-                  <h2 className="mt-10">All Movies</h2>
+        <div className="wrapper">
+          <header>
+            <img src="./hero.png" alt="Hero Banner" />
+            <h1>
+              Find <span className="text-gradient">Movies</span> You'll Enjoy
+              Without the Hassle
+            </h1>
 
-                  {isLoading ? (
-                     <Spinner />
-                  ) : errorMessage ? (
-                     <p className="text-red-500">{errorMessage}</p>
-                  ) : (
-                     <ul>
-                        {movieList.map((movie) => (
-                           <MovieCard key={movie.id} movie={movie} />
-                        ))}
-                     </ul>
-                  )}
-               </section>
-            </div>
-         </main>
+            <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+          </header>
 
-         <footer className="flex justify-between p-4">
-            <p className="text-center text-sm text-gray-500">
-               &copy; {new Date().getFullYear()} Adam Liu. All rights reserved.
-            </p>
-            <div className="flex space-x-4">
-               <a
-                  className="text-center text-sm text-gray-500"
-                  href="https://github.com/ting-haoliu/react-movie_searching"
-                  target="_blank"
-               >
-                  GitHub
-               </a>
-               <a
-                  className="text-center text-sm text-gray-500"
-                  href="https://www.linkedin.com/in/tinghao-liu/"
-                  target="_blank"
-               >
-                  LinkedIn
-               </a>
-            </div>
-         </footer>
-      </>
-   );
+          {trendingMovies.length > 0 && (
+            <section className="trending">
+              <h2>Trending Movies</h2>
+
+              <ol>
+                {trendingMovies.map((movie, index) => (
+                  <li key={movie.$id}>
+                    <p>{index + 1}</p>
+                    <Link to={`/movie/${movie.movie_id}`}>
+                      <img
+                        src={movie.poster_url}
+                        alt={`Poster of ${movie.title}`}
+                      />
+                      <span className="sr-only">{movie.title}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ol>
+            </section>
+          )}
+
+          <section className="all-movies">
+            <h2 className="mt-10">All Movies</h2>
+
+            {isLoading ? (
+              <Spinner />
+            ) : errorMessage ? (
+              <p className="text-red-500">{errorMessage}</p>
+            ) : (
+              <ul>
+                {movieList.map((movie) => (
+                  <MovieCard key={movie.id} movie={movie} />
+                ))}
+              </ul>
+            )}
+          </section>
+        </div>
+      </main>
+
+      <footer className="flex justify-between p-4">
+        <p className="text-center text-sm text-gray-500">
+          &copy; {new Date().getFullYear()} Adam Liu. All rights reserved.
+        </p>
+        <nav className="flex space-x-4">
+          <a
+            className="text-center text-sm text-gray-500"
+            href="https://github.com/ting-haoliu/react-movie_searching"
+            target="_blank"
+          >
+            GitHub
+          </a>
+          <a
+            className="text-center text-sm text-gray-500"
+            href="https://www.linkedin.com/in/tinghao-liu/"
+            target="_blank"
+          >
+            LinkedIn
+          </a>
+        </nav>
+      </footer>
+    </>
+  );
 };
 
 export default HomePage;
